cbdb3a97ca4afb8f14bd8db8146a10d1fa5c9607
Jason A. Evans <jasonevans77@gmail.com>
2025-10-04 16:10:22 -0400

    Updated Excel and CSV files.

--- song_title_set/song_title_set.py
+++ song_title_set/song_title_set.py
@@ -1,13 +1,13 @@
 # Jason Evans
-# 2025-05-15
+# 2025-10-04
 # Python script to read a path and song title from a | separated file.
 # Song title is set.
 #
 # Command Line Arguments:
-# 1: String representing drive letter. Example "D"
-# 2: int representing number of pad characters in flac filename before date. Example: gd1986-12-15 = 2
-# 3: int representing if this is a Grateful Dead (1) or Jerry Garcia (0) lookup
-# 4: int. (1) for simple set, meaning just use | separated file (no database, etc.). (0) for pulling date from folder name
+# 1: String representing drive letter where your library resides. Example "D".
+# 2: int representing number of pad characters in flac filename before date. Example: gd1986-12-15 = 2.
+# 3: int representing if this is a Grateful Dead (1) or Jerry Garcia (0) lookup.
+# 4: int. (1) for simple set, meaning just use | separated file (no database, etc.). (0) for pulling date from folder name.
 #    and using database.
 # 5: Path to | separated file.
 
@@ -49,7 +49,7 @@ if os.path.exists(too_many_songs_log): os.remove(too_many_songs_log)
 
 def write_line(file, line):
     f = open(file, 'a')
-    
+
     line = line.strip('\r')
     line = line.strip('\n')
     line += '\n'
@@ -60,40 +60,42 @@ with open(infile, "r") as file:
     con = sqlite3.connect(db) #DB connection
     cur = con.cursor()
     YearEcho = ""
-    
+
     for line in file:
         items = line.split('|')
-        
+
         songfile = str(items[0]).replace("[drive]", Drive)
         songtitle = str(items[1])
         songsegue = str(items[2]).strip('\n')
-        
+
         try:
             audio = music_tag.load_file(songfile)
         except FileNotFoundError:
             write_line(file_error, songfile)
             continue
-        
+
         #Always just using the song title from the CSV.
         NewSongTitle = songtitle
         segue = ""
-        
+
+        #I am setting a segue if the file contains one OR the database contains one.
+        if songsegue == ">":
+            segue = " >"
+
         if SimpleSet == 1:
-            if songsegue == ">": 
-                segue = " >"
-                
-            audio['TITLE'] = songtitle + segue
-            audio.save()
-            
+            if audio['TITLE'] != (songtitle + segue): #Only write if it's different so I don't change the file date.
+                audio['TITLE'] = songtitle + segue
+                audio.save()
+
             continue
-        
+
         #Pull show folder in order to isolate date and SHNID
         ShowFolder = pathlib.PurePath(songfile).parent.name
-                
+
         #Attempt to pull SHNID from folder name in order to keep a record.
         words = ShowFolder.split(".")
         SHNID = ""
-        
+
         for word in words:
             try:
                 SHNID_int = int(word)
@@ -101,47 +103,45 @@ with open(infile, "r") as file:
                 write_line(SHNID_log, SHNID)
             except ValueError:
                 continue
-        
+
         #Get date from song file name.
         #gd1971-02-20 for example
         ParsedDate = ShowFolder[NumPadChars:NumPadChars + 10]
         DateFormat = "%Y-%m-%d"
-        
+
         try:
             dt = datetime.datetime.strptime(ParsedDate, DateFormat) #parse(DateFolder[:(NumPadChars + 10)], fuzzy = False) #Date is 10 chars
         except:
             write_line(date_not_found_log, songfile)
-            
+
             #This usually means the date in the folder name has an XX or 00 in it, meaning unknown.
-            #Just set based on file.
-            if songsegue == ">": 
-                segue = " >"
-                
-            audio['TITLE'] = songtitle + segue
-            audio.save()
-            
+            if audio['TITLE'] != (songtitle + segue): #Only write if it's different so I don't change the file date.
+                audio['TITLE'] = songtitle + segue
+                audio.save()
+
             continue
-        
+
         year = dt.year
         month = dt.month
         day = dt.day
-        
+
         #Echo new years
         if YearEcho != str(year):
             YearEcho = str(year)
-            print(YearEcho)        
-        
+            print(YearEcho)
+
         #Handle Early/Late shows
         EarlyLate = None
         result = None
-        
+
         if "early" in ShowFolder.lower() and "late" in ShowFolder.lower(): #Rare instance where filename contains both early and late
             write_line(skipped_log, songfile)
-            
+
             #Similar to no date found, just use file.
-            audio['TITLE'] = songtitle
-            audio.save()
-            
+            if audio['TITLE'] != (songtitle + segue): #Only write if it's different so I don't change the file date.
+                audio['TITLE'] = songtitle + segue
+                audio.save()
+
             continue
         elif "early" in ShowFolder.lower():
             EarlyLate = "EARLY"
@@ -153,7 +153,7 @@ with open(infile, "r") as file:
                   "FROM acts INNER JOIN (songs INNER JOIN ((events INNER JOIN event_sets ON events.id = event_sets.event_id) INNER JOIN event_songs ON event_sets.id = event_songs.event_set_id) ON songs.id = event_songs.song_id) ON acts.id = events.act_id " \
                   "WHERE event_sets.soundcheck=0 AND acts.gd=? AND events.year=? AND events.month=? AND events.day=? AND songs.name=? " \
                   "ORDER BY event_sets.seq_no, event_songs.seq_no"
-            
+
             result = cur.execute(sql, (GD, year, month, day, songtitle)).fetchall()
         else:
             sql = "SELECT songs.name, event_songs.segue " \
@@ -162,34 +162,25 @@ with open(infile, "r") as file:
                   "ORDER BY event_sets.seq_no, event_songs.seq_no"
 
             result = cur.execute(sql, (GD, year, month, day, songtitle, EarlyLate)).fetchall()
-        
-        
+
         if len(result) == 0: #No songs
             write_line(song_not_found_log, songfile + '|' + songtitle)
-            
+
             #This is probably a title that isn't an actual song, for example 'Banter'.
             #Could also be a line that has '>' in the middle since it's actually multiple songs segued.
-            #I'm deciding to pull forward segues from the original titles.
-            if songsegue == ">": 
-                segue = " >"
 
         elif len(result) > 1: #Too many songs
             write_line(too_many_songs_log, songfile + '|' + songtitle)
-            
+
             #This is usually a case where a song is played twice, for example a reprise.
-            #The issue is that I can't figure out an accurate way to handle the second segue.
-            #I'm deciding to pull forward segues from the original titles.
-            if songsegue == ">": 
-                segue = " >"
-             
+            #This could create a segue error.
+
         else:
             row = result[0]
 
             if row[1] == 1:
-                segue = " >"
-            else:
-                if songsegue == ">": #I'm deciding to pull forward segues from the original titles.
-                    segue = " >"
-        
-        audio['TITLE'] = NewSongTitle + segue
-        audio.save()
+                segue = " >" #Setting based on database, but coult have already been set based on file.
+
+        if audio['TITLE'] != (songtitle + segue): #Only write if it's different so I don't change the file date.
+            audio['TITLE'] = songtitle + segue
+            audio.save()
